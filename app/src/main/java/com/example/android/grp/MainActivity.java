6095f0991e8d9adcddcd5a98f6971e40e6be0898
AmulVerma <39342187+AmulVerma@users.noreply.github.com>
2019-02-10 20:33:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/grp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.android.grp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.imageButton2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
